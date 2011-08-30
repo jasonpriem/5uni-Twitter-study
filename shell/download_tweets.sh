bf03a315d5d54738742be0ed2fd2ebd79ecc8c19
Jason Priem <jason@jasonpriem.com>
2011-08-30 19:59:44 -0400

    changed dir structure, download scripts

--- shell/download_tweets.sh
+++ shell/download_tweets.sh
@@ -1,2 +1,2 @@
 read -p "enter password: " pw
-curl https://alm:$pw@alm.cloudant.com/five_uni_twitter/_design/main/_list/csv/tweets > /home/jason/projects/5uni_twitter/tweets/tweets_from_db.csv
+curl https://alm:$pw@alm.cloudant.com/five_uni_twitter/_design/main/_list/csv/tweets > /home/jason/projects/5uni-twitter/data/tweets_from_db.csv
